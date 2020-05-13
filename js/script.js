6f37ca44675241bf5f8e23d10af4ed722c368683
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2020-05-13 10:52:10 +0300

    Работающий счетчик

--- js/script.js
+++ js/script.js
@@ -25,4 +25,19 @@ function checkScroll(scrollPos,introH){
       nav.removeClass("fixed");
     }
 }
+//Счетчик цифр
+$('.counter__number').each(function(){
+$(this).prop('Counter',0).animate({
+  Counter:$(this).text()
+},{
+  duration: 4000,
+  easing:'swing',
+  step:function(now){
+ $(this).text(Math.ceil(now));
+  }
+});
+});
+
+
+
 });//закрывает все функции
