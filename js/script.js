7b7b925035cc2d0a448d403ab35f314ca5ad4db9
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2020-05-13 21:41:05 +0300

    3д карточки

--- js/script.js
+++ js/script.js
@@ -26,11 +26,12 @@ function checkScroll(scrollPos,introH){
     }
 }
 //Счетчик цифр
+
 $('.counter__number').each(function(){
 $(this).prop('Counter',0).animate({
   Counter:$(this).text()
 },{
-  duration: 4000,
+  duration: 7000,
   easing:'swing',
   step:function(now){
  $(this).text(Math.ceil(now));
