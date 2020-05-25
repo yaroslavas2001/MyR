50223649036c38b8b7a4cb9265faa2f1b5541374
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2020-05-25 11:30:59 +0300

    рабочий слайдер

--- js/script.js
+++ js/script.js
@@ -39,6 +39,23 @@ $(this).prop('Counter',0).animate({
 });
 });
 
+$('.slider__row').slick({
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
 
+var filtered = false;
+
+$('.js-filter').on('click', function(){
+  if (filtered === false) {
+    $('.filtering').slick('slickFilter',':even');
+    $(this).text('Unfilter Slides');
+    filtered = true;
+  } else {
+    $('.filtering').slick('slickUnfilter');
+    $(this).text('Filter Slides');
+    filtered = false;
+  }
+});
 
 });//закрывает все функции
