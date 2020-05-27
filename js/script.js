c6f8d1112a5c3b7ba93d04978c7924ba529db2b3
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2020-05-27 21:16:57 +0300

    мобильное меню

--- js/script.js
+++ js/script.js
@@ -57,5 +57,28 @@ $('.js-filter').on('click', function(){
     filtered = false;
   }
 });
+$('.slider__row__mobile').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2
+});
 
+var filtered = false;
+
+$('.js-filter').on('click', function(){
+  if (filtered === false) {
+    $('.filtering').slick('slickFilter',':even');
+    $(this).text('Unfilter Slides');
+    filtered = true;
+  } else {
+    $('.filtering').slick('slickUnfilter');
+    $(this).text('Filter Slides');
+    filtered = false;
+  }
+});
+
+$('.menu-btn').on('click',function(even){
+even.preventDefault;
+$(this).toggleClass('menu-btn_active');
+$('.menu__nav').toggleClass('menu__nav_active');
+});
 });//закрывает все функции
